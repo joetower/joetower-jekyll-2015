86a9216bf1f1ef2522c03fc16fea047db3ab692e
Joe Tower <joetower@gmail.com>
2014-08-16 14:46:32 -0500

    :carousel_horse: Fixed this repo by properly adding a Gemfile, .ruby-version, and trying to prep to change from grunt to gulp.

--- config.rb
+++ config.rb
@@ -1,8 +1,9 @@
 # Require any additional compass plugins here.
+require 'toolkit'
 require 'compass'
 require 'singularitygs'
 require 'breakpoint'
-require 'toolkit'
+
 
 # Set this to the root of your project when deployed:
 http_path = "/"
