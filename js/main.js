2fe816726ae61ca66b2a664a5c38b52148e8ccc4
Joe Tower <joetower@gmail.com>
2015-11-27 12:13:36 -0600

    Adjusted animation of browse by category lis

--- js/main.js
+++ js/main.js
@@ -17,12 +17,18 @@
       event.preventDefault();
       if ($('.toggle-it').hasClass('active')) {
         $('.toggle-it').removeClass('active').addClass('inactive');
+        $('.toggle-it ul').removeClass('fade-in').addClass('fade-out');
+        $('.toggle-it li a').removeClass('fade-in').addClass('fade-out');
         $('.toggle').removeClass('active').addClass('inactive');
       } else {
         $('.toggle').removeClass('inactive').addClass('active');
         $('.toggle-it').removeClass('inactive').addClass('active');
+        $('.toggle-it ul').removeClass('fade-out').addClass('fade-in');
+        $('.toggle-it li a').removeClass('fade-out').addClass('fade-in');
       }
     });
+
+
   });
 
 })(jQuery);
